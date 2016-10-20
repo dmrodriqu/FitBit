240724f2be6388c72d0892b9d217ad993678828e
Dylan Rodriquez <dylanmr@uchicago.edu>
2016-10-20 16:20:02 -0500

    abstraction of location functions

--- parseup.py
+++ parseup.py
@@ -13,6 +13,7 @@ from pandas.io.json import json_normalize
 
 path = '/Users/Dylan/Dropbox/FitBit/data_20161012'
 
+
 # data organized in the following manner:
 # ID_1_dir
 # ----sleep_json_file
@@ -29,7 +30,8 @@ path = '/Users/Dylan/Dropbox/FitBit/data_20161012'
 # ----survey_json_file
 
 def createdataframe(path, datatofind):
-    # setup of DIVIDE AND CONQUER for creation of dictionary of directory and subdirectory of patient information
+    # type: (string, string) -> np.dataFrame
+    # setup of divide and conquer for creation of dictionary of directory and subdirectory of patient information
     # set json keys and path values
     string_to_search_for = ''
     json_key = ''
@@ -44,11 +46,13 @@ def createdataframe(path, datatofind):
 
     # path  --> list of directories in data_1
     def directList(path):
+        # type: (string) -> [string]
         directoryList = [x for x in os.listdir(path) if os.path.isdir(x) and x != '.git']
         return directoryList
 
     # path  --> list of subdirectories in data_1
     def directNestList(path):
+        # type: (string) -> [string]
         sleepfiles = []
         fileList = [os.listdir(x) for x in directList(path)]
         for sublist in fileList:
@@ -90,7 +94,7 @@ def createdataframe(path, datatofind):
     # [filepath] --> JSON data object
     all_frames = []
     # condition block if sleep or step here
-    ## abstract to function later
+    # abstract to function later
     if datatofind == 'survey':
         df = []
         for json_file in list_of_json_files:
@@ -129,30 +133,31 @@ survey_data = createdataframe(path, 'survey')
 
 
 
-#for location_arrays in survey_data['location'][0]:
-#    for location_dictionary in location_arrays:
-#        print location_dictionary
+def get_gps(survey_dataframe):
+    # type: (np.dataFrame) -> [{'string':int, 'string':{'string':float, 'string':float}}]
+    location_dictionary_array = []
+    i = 0
+    while i < len(survey_data):
+        location_dictionary_array.append([x for x in survey_data['location']][i])
+        i += 1
+    return location_dictionary_array
 
-loc_0 = [x for x in survey_data['location']][0]
 
-loc_1 = [x for x in survey_data['location']][1]
-loc_2 = [x for x in survey_data['location']][2]
+testing = get_gps(survey_data)
 
-# find n-1, and n+1 slopes, average together
+def gps_to_distance(locationdictionary, index):
+    # type: [{'string':int, 'string':{'string':float, 'string':float}}] -> [float],[float],[int]
+    array_of_longitudes = []
+    array_of_latitudes = []
+    array_of_time = []
+    # just one at a time, iterate with function
+    for x in locationdictionary[index]:
+        array_of_latitudes.append(x[u'Value'][u'Lat'])
+        array_of_longitudes.append(x[u'Value'][u'Lng'])
+        array_of_time.append(x[u'TimeCompleted']/1000)
+    return array_of_latitudes, array_of_longitudes, array_of_time
 
-# change in distance == euclidean distance
-
-# distance=sqrt((lat1 + lat2)**2 + (lon1 + lon2)**2)
-# array of time is UnixTimestamp
-array_of_longitudes = []
-array_of_latitudes = []
-array_of_time = []
-
-for x in loc_2:
-    array_of_latitudes.append(x[u'Value'][u'Lat'])
-    array_of_longitudes.append(x[u'Value'][u'Lng'])
-    array_of_time.append(x[u'TimeCompleted'])
-
+euclidlist =  gps_to_distance(testing, 0)
 
 
 # latitude_array, longitude_array, time_list -->[average velocity]
@@ -170,46 +175,51 @@ def euclid_slope_lat_long(latitude_array, longitude_array, time_list):
     index_of_n = 1
     times = []
     velocities = []
+
     def haversine(lat1, lat2, lon1, lon2):
         # type: (float, float, float, float) -> float
-        r = 6371
-        lat1, lat2, lon1, lon2 =  map(math.radians, [lat1, lat2, lon1, lon2])
-        lat_part = math.sin((lon2 - lon1)/2)**2
-        lon_part = math.sin((lat2 - lat1)/2)**2
+        r = 3959
+        lat1, lat2, lon1, lon2 = map(math.radians, [lat1, lat2, lon1, lon2])
+        lat_part = math.sin((lon2 - lon1) / 2) ** 2
+        lon_part = math.sin((lat2 - lat1) / 2) ** 2
         sqrt_part = (lon_part + math.cos(lat1) * math.cos(lat2) * lat_part) ** (0.5)
-        return 2*r*math.asin(sqrt_part)
-    while index_of_n + 1< len(latitude_array):
-        distance =  haversine(latitude_array[index_of_n], latitude_array[index_of_n + 1],
-                              longitude_array[index_of_n], longitude_array[index_of_n + 1])
-        time_difference_a = time_list[index_of_n+1] - time_list[index_of_n]
-        time_difference_a = time_difference_a / (3600.00 * 1000)
+        return 2 * r * math.asin(sqrt_part)
+
+    while index_of_n + 1 < len(latitude_array):
+        distance = haversine(latitude_array[index_of_n], latitude_array[index_of_n + 1],
+                             longitude_array[index_of_n], longitude_array[index_of_n + 1])
+        time_difference_a = time_list[index_of_n + 1] - time_list[index_of_n]
+        time_difference_a = time_difference_a / (3600.00)
         velocities.append(distance / time_difference_a)
-        times.append(int(str(time_list[index_of_n])[0:-3]))
-        index_of_n +=1
+        times.append(int(str(time_list[index_of_n])))
+        index_of_n += 1
     return times, velocities
 
+data = euclid_slope_lat_long(euclidlist[0], euclidlist[1], euclidlist[2])
 import collections
 
-data =  euclid_slope_lat_long(array_of_latitudes, array_of_longitudes, array_of_time)
+
 import time
+
 from bokeh.plotting import figure, output_file, show
 from bokeh.plotting import *
 import numpy as np
-#from bokeh.plotting import figure, output_file, show
-#print data
+
+# from bokeh.plotting import figure, output_file, show
+# print data
 x = data[0]
-#x = (map(lambda x: datetime.datetime.fromtimestamp(int(x)).strftime('%Y-%m-%d'),data[0]))
-print x
+# x = (map(lambda x: datetime.datetime.fromtimestamp(int(x)).strftime('%Y-%m-%d'),data[0]))
+#print x
 y = data[1]
 
 # output to static HTML file
 output_file("lines.html")
 
 # create a new plot with a title and axis labels
-p = figure(title="Velocity Vs Time Using Raw GPS Data", x_axis_label='Date Time', y_axis_label='KPH', )
+p = figure(title="Velocity Vs Time Using Raw GPS Data", x_axis_label='Date Time', y_axis_label='MPH', )
 
 # add a line renderer with legend and line thickness
 p.line(x, y, legend="Velocity.", line_width=2)
-p.xaxis.major_label_orientation = np.pi/4
+p.xaxis.major_label_orientation = np.pi / 4
 # show the results
 show(p)
