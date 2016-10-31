7266a976eda36530663bcffedd28f4c7eeba8feb
Dylan Rodriquez <dylanmr@uchicago.edu>
2016-10-31 13:25:31 -0500

    abstracted more classes

--- parseup.py
+++ parseup.py
@@ -31,12 +31,11 @@ fpath = '/Users/Dylan/Dropbox/FitBit/data_20161012'
 # ----survey_json_file
 
 class Table:
-
     def __init__(self):
         self.listofframes = []
         self.dataType = ''
 
-    def createdataframe(self, path, datatofind):
+    def parseJSON(self, path, datatofind):
         # type: (string, string) -> np.dataFrame
         # setup of divide and conquer for creation of dictionary of directory and subdirectory of patient information
         # set json keys and path values
@@ -59,7 +58,7 @@ class Table:
         # path  --> list of directories in data_1
         def directList(path):
             # type: (string) -> [string]
-            directoryList = [path+'/'+str(x) for x in os.listdir(path) if os.path.isdir(path + '/' + str(x))]
+            directoryList = [path + '/' + str(x) for x in os.listdir(path) if os.path.isdir(path + '/' + str(x))]
             return directoryList
 
         # path  --> list of subdirectories in data_1
@@ -104,7 +103,7 @@ class Table:
             list_of_json_files.append(f)
             dictionary_index += 1
         # [filepath] --> JSON data object
-        #self.listofframes = []
+        # self.listofframes = []
         # condition block if sleep or step here
         # abstract to function later
         if datatofind == 'survey':
@@ -113,7 +112,6 @@ class Table:
                 data = json.loads(json_to_parse)
                 # data.key[0]: data.value[1][0], # key[1]: value[1][1], # key[2]: value[1][2]...# key[n]: value[1][n]
                 self.listofframes.append(json_normalize(data))
-
                 # JSON --> [pd.DataFrame]
 
         else:
@@ -133,96 +131,82 @@ class Table:
                 singleframe['id'] = json_file
                 self.listofframes.append(singleframe)
 
-    def concatinateDataFrames(self):
+    def getDataFrame(self):
         return pd.concat(self.listofframes).reset_index()
 
-
-    def get_gps(self, tableFrame):
+    def getGPS(self, tableFrame):
         # type: (np.dataFrame) -> [{'string':int, 'string':{'string':float, 'string':float}}]
         location_dictionary_array = []
         i = 0
-        while i < len(df):
+        while i < len(tableFrame):
             location_dictionary_array.append([x for x in tableFrame['location']][i])
             i += 1
         return location_dictionary_array
 
+    def gps_rdt(self, locationDictionary, index):
+        # type: [{'string':int, 'string':{'string':float, 'string':float}}] -> [float],[float],[int]
+        array_of_longitudes = []
+        array_of_latitudes = []
+        array_of_time = []
+        # just one at a time, iterate with function
+        for x in locationDictionary[index]:
+            array_of_latitudes.append(x[u'Value'][u'Lat'])
+            array_of_longitudes.append(x[u'Value'][u'Lng'])
+            array_of_time.append(x[u'TimeCompleted'] / 1000)
+        return array_of_latitudes, array_of_longitudes, array_of_time
+
+    # latitude_array, longitude_array, time_list -->[average velocity]
+    def euclid_slope_lat_long(self, latitude_array, longitude_array, time_list):
+        # method -->
+        # start n=1
+        #
+        #  xn - x(n-1)
+        # -------------
+        #  tn-t(n-1)
+        # repeat for n and n + 1
+        # average
+        # use as t(n)
+        # x(n) is the haversine formula
+        index_of_n = 1
+        times = []
+        velocities = []
+
+        def haversine(lat1, lat2, lon1, lon2):
+            # type: (float, float, float, float) -> float
+            r = 3959
+            lat1, lat2, lon1, lon2 = map(math.radians, [lat1, lat2, lon1, lon2])
+            lat_part = math.sin((lon2 - lon1) / 2) ** 2
+            lon_part = math.sin((lat2 - lat1) / 2) ** 2
+            sqrt_part = (lon_part + math.cos(lat1) * math.cos(lat2) * lat_part) ** (0.5)
+            return 2 * r * math.asin(sqrt_part)
+
+        # similar to fibonacci alg. change to address efficiency.
+        # create array of size n
+        # iterate over distances, change values of array
+        while index_of_n + 1 < len(latitude_array):
+            distance = haversine(latitude_array[index_of_n], latitude_array[index_of_n + 1],
+                                 longitude_array[index_of_n], longitude_array[index_of_n + 1])
+            time_difference_a = time_list[index_of_n + 1] - time_list[index_of_n]
+            time_difference_a = time_difference_a / (3600.00)
+            velocities.append(distance / time_difference_a)
+            times.append(int(str(time_list[index_of_n])))
+            index_of_n += 1
+        return times, velocities
+
 
 table1 = Table()
-table1.createdataframe(fpath, 'survey')
-frame = table1.concatinateDataFrames()
-print table1.get_gps(frame)
+table1.parseJSON(fpath, 'survey')
+frame = table1.getDataFrame()
+gps = table1.getGPS(frame)
+gps1 = table1.gps_rdt(gps, 0)
+#distance1 = table1.gps_to_distance(gps1,0)
+euclidiangps1 = table1.euclid_slope_lat_long(gps1[0], gps1[1], gps1[2])
+print euclidiangps1
 
 
 
-
-
-def get_gps(survey_dataframe):
-    # type: (np.dataFrame) -> [{'string':int, 'string':{'string':float, 'string':float}}]
-    location_dictionary_array = []
-    i = 0
-    while i < len(survey_data):
-        location_dictionary_array.append([x for x in survey_data['location']][i])
-        i += 1
-    return location_dictionary_array
-
-
-testing = get_gps(survey_data)
-
-def gps_to_distance(locationdictionary, index):
-    # type: [{'string':int, 'string':{'string':float, 'string':float}}] -> [float],[float],[int]
-    array_of_longitudes = []
-    array_of_latitudes = []
-    array_of_time = []
-    # just one at a time, iterate with function
-    for x in locationdictionary[index]:
-        array_of_latitudes.append(x[u'Value'][u'Lat'])
-        array_of_longitudes.append(x[u'Value'][u'Lng'])
-        array_of_time.append(x[u'TimeCompleted']/1000)
-    return array_of_latitudes, array_of_longitudes, array_of_time
-
-euclidlist =  gps_to_distance(testing, 0)
-
-
-# latitude_array, longitude_array, time_list -->[average velocity]
-def euclid_slope_lat_long(latitude_array, longitude_array, time_list):
-    # method -->
-    # start n=1
-    #
-    #  xn - x(n-1)
-    # -------------
-    #  tn-t(n-1)
-    # repeat for n and n + 1
-    # average
-    # use as t(n)
-    # x(n) is the haversine formula
-    index_of_n = 1
-    times = []
-    velocities = []
-
-    def haversine(lat1, lat2, lon1, lon2):
-        # type: (float, float, float, float) -> float
-        r = 3959
-        lat1, lat2, lon1, lon2 = map(math.radians, [lat1, lat2, lon1, lon2])
-        lat_part = math.sin((lon2 - lon1) / 2) ** 2
-        lon_part = math.sin((lat2 - lat1) / 2) ** 2
-        sqrt_part = (lon_part + math.cos(lat1) * math.cos(lat2) * lat_part) ** (0.5)
-        return 2 * r * math.asin(sqrt_part)
-    # similar to fibonacci alg. change to address efficiency.
-    # create array of size n
-    # iterate over distances, change values of array
-    while index_of_n + 1 < len(latitude_array):
-        distance = haversine(latitude_array[index_of_n], latitude_array[index_of_n + 1],
-                             longitude_array[index_of_n], longitude_array[index_of_n + 1])
-        time_difference_a = time_list[index_of_n + 1] - time_list[index_of_n]
-        time_difference_a = time_difference_a / (3600.00)
-        velocities.append(distance / time_difference_a)
-        times.append(int(str(time_list[index_of_n])))
-        index_of_n += 1
-    return times, velocities
-
-#data = euclid_slope_lat_long(euclidlist[0], euclidlist[1], euclidlist[2])
+# data = euclid_slope_lat_long(euclidlist[0], euclidlist[1], euclidlist[2])
 import collections
-
 
 import time
 
@@ -232,22 +216,22 @@ import numpy as np
 
 # from bokeh.plotting import figure, output_file, show
 # print data
-#x = data[0]
+# x = data[0]
 # x = (map(lambda x: datetime.datetime.fromtimestamp(int(x)).strftime('%Y-%m-%d'),data[0]))
-#print x
-#y = data[1]
+# print x
+# y = data[1]
 
 # output to static HTML file
-#output_file("lines.html")
+# output_file("lines.html")
 
 # create a new plot with a title and axis labels
-#p = figure(title="Velocity Vs Time Using Raw GPS Data", x_axis_label='Date Time', y_axis_label='MPH', )
+# p = figure(title="Velocity Vs Time Using Raw GPS Data", x_axis_label='Date Time', y_axis_label='MPH', )
 
 # add a line renderer with legend and line thickness
-#p.line(x, y, legend="Velocity.", line_width=2)
-#p.xaxis.major_label_orientation = np.pi / 4
+# p.line(x, y, legend="Velocity.", line_width=2)
+# p.xaxis.major_label_orientation = np.pi / 4
 # show the results
-#show(p)
+# show(p)
 
 # get IDs from filepaths, create new colums for patient ID
 ID_array = []
@@ -258,9 +242,7 @@ for each in step_data['id']:
     ID_array.append(matches[3])
 step_data['ID'] = ID_array
 
-
 # last minute graphing
-# I know triple quotes arent supposed to be used for block comment
 '''ID_array = []
 for each in sleep_data['id']:
     regex = r"(?<=\W)[a-zA-Z0-9]{6}"
