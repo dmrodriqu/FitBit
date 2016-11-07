4e1e6d376e4905385854b5471681280e335fe863
Dylan <dylanmr@uchicago.edu>
2016-11-07 14:39:09 -0600

    workflow for concatenating frames

--- parser.py
+++ parser.py
@@ -13,6 +13,16 @@ from pandas.io.json import json_normalize
 fpath = '/Users/Dylan/Dropbox/FitBit/UChicagoIBD_data.json'
 
 
+def parseJsonFile(path):
+    json_to_parse = open(path).read()
+    data = json.loads(json_to_parse)
+    subjectIDs = data.keys()
+    fullFrame = pd.read_json(json_to_parse, orient='index')
+    fullFrame['ID'] = fullFrame.index
+    return fullFrame.reset_index()
+
+parsedJson = parseJsonFile(fpath)
+
 class Table:
 
     def __init__(self):
@@ -22,14 +32,6 @@ class Table:
         self.listOfExpandedFrames = []
         self.stepFramesToConcatenate = []
         self.heartFramesToConcatenate = []
-
-    def parseJsonFile(self, path):
-        json_to_parse = open(path).read()
-        data = json.loads(json_to_parse)
-        subjectIDs = data.keys()
-        fullFrame = pd.read_json(json_to_parse, orient='index')
-        fullFrame['ID'] = fullFrame.index
-        return fullFrame.reset_index()
 
 
     def recursiveRows(self, dataFrame, dataType):
@@ -68,6 +70,7 @@ class Table:
         return pd.concat(self.listOfExpandedFrames).reset_index()
 
     def createStepOrHeartColumns(self, originalDataFrame, seriesToExpand, seriesName):
+        stepFramesToConcatenate = []
         indexCount = 0
         while indexCount < len(seriesToExpand):
             for x in seriesToExpand:
@@ -83,17 +86,27 @@ class Table:
                 indexCount += 1
         if len(self.stepFramesToConcatenate)> 0:
             return pd.concat(self.stepFramesToConcatenate).reset_index()
-        if len(self.heartFramesToConcatenate)>0:
+        elif len(self.heartFramesToConcatenate)>0:
             return pd.concat(self.heartFramesToConcatenate).reset_index()
 
 
-new = Table()
-table = new.parseJsonFile(fpath)
-# Heart, Steps, Sleep
-heartrows = new.recursiveRows(table, 'Heart')
-print new.createStepOrHeartColumns(table, heartrows, 'Heart')
-steprows = new.recursiveRows(table, 'Steps')
-#print steprows
-print new.createStepOrHeartColumns(table, steprows, 'Steps')
 
-print len(new.stepFramesToConcatenate)
+def concatStepHeartSleep(fileToJson):
+    # Heart
+    heartTable = Table()
+    parsedJsonforHeartTable = parsedJson
+    heartRows = heartTable.recursiveRows(parsedJsonforHeartTable, 'Heart')
+    print heartRows
+    # Steps
+    sleepTable = Table()
+    parsedJsonForSleepTable = parsedJson
+    sleepRows = sleepTable.recursiveRows(parsedJsonForSleepTable, 'Sleep')
+    print sleepRows
+    # Sleep
+    stepTable = Table()
+    parsedJsonforStepTable = parsedJson
+    stepsRows = stepTable.recursiveRows(parsedJsonforStepTable, 'Steps')
+    print stepsRows
+
+concatStepHeartSleep(fpath)
+
