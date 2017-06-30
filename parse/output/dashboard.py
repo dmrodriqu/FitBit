c6dfa980867aa03f75e3b5fff1663aa410e63c94
Dylan Rodriquez <dylanmr@uchicago.edu>
2017-06-30 11:56:46 -0500

    Inserted all skipdates

--- parse/output/dashboard.py
+++ parse/output/dashboard.py
@@ -1,5 +1,6 @@
 from findOmissions import MainData
 import pandas as pd
+import datetime
 
 class Results:
 
@@ -40,16 +41,37 @@ class Results:
 		self.Dates = surveyDates
 		self.Values = surveyValues
 
+	def insertDateGaps(self, dates):
+		for k, v in dates.iteritems():
+			i = 0
+			while i < len(v) - 1:
+				print v
+				try:
+					dateDiff = datetime.datetime.strptime(v[i+1][0] , "%Y-%m-%d") - datetime.datetime.strptime(v[i][0], "%Y-%m-%d")
+					print dateDiff
+				except:
+					i+=1
+				if dateDiff > datetime.timedelta(days = 1):
+					repeat = 0
+					while repeat < int(dateDiff.days - 1):
+						v.insert(i+1, 'SKIPPED')
+						repeat += 1
+					i += int(dateDiff.days) 
+				else:
+					i+=1
+		return dates
 	
 	def output(self, date = True, values = False):
 		a =  pd.DataFrame.from_dict(self.Values, orient = 'index')
-		b = pd.DataFrame.from_dict(self.Dates,  orient = 'index')
+		b = pd.DataFrame.from_dict(self.insertDateGaps(self.Dates),  orient = 'index')
+		print b
+
 		if values:
-			return a
+			return a.to_csv('{}.csv'.format(self.survey))
 		else: 
-			return b
+			return b.to_csv('{}.csv')
 		
 
-# usage 
-#result = Results('PSQI')
-#print result.output()
+#usage 
+result = Results('WongBaker')
+print result.output(values = False)
